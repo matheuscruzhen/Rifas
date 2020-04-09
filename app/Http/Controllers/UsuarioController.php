37030cb6e5c7ac73ee8acf81c56f209c0418499f
Jose Murillo <jooseemurillo@gmail.com>
2020-04-09 13:58:28 -0300

    Create: Funcionalidade de login ✨

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -1,20 +1,36 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Usuario;
+use DB;
 use Illuminate\Http\Request;
 
 class UsuarioController extends Controller
 {
-    function login() {
-        return view('login');
-    }
 
     function logar(Request $request) {
         $username = $request->username;
         $senha = $request->senha;
 
-        // var_dump();
+        $usuario = DB::select('SELECT * FROM usuarios WHERE username = :username AND senha = :senha',
+            [
+                'username' => $username,
+                'senha' => $senha
+            ]
+            );
+
+        if($usuario[0]->ativo && $usuario[0]->tipousuario_id == 1) {
+            return redirect('/admin/dashboard');
+        }
+
+        if($usuario[0]->ativo && $usuario[0]->tipousuario_id == 2) {
+            return redirect('/gerente/dashboard');
+        }
+
+        if($usuario[0]->ativo && $usuario[0]->tipousuario_id == 3) {
+            return redirect('/cambista/dashboard');
+        }
+
+        echo "Sua conta está desativada ou seus dados estam incorretos";
+
     }
 }
