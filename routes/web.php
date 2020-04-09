21c59bb58aa4149356378542cdd31f29433ca260
Jose Murillo <jooseemurillo@gmail.com>
2020-04-09 13:57:21 -0300

    Update: criando os grupos de rotas ✨

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,54 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Rotas direcionadas somente ao ADMIN do sistema
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+*/
+Route::prefix('admin')->group(function () {
+    Route::get('dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+/*
+|--------------------------------------------------------------------------
+| Rotas direcionadas somente aos GERENTES do sistema
+|--------------------------------------------------------------------------
+*/
+Route::prefix('gerente')->group(function () {
+    Route::get('dashboard', function () {
+        return view('gerente.dashboard');
+    });
+});
+
+/*
+|--------------------------------------------------------------------------
+| Rotas direcionadas somente aos GERENTES do sistema
+|--------------------------------------------------------------------------
+*/
+Route::prefix('cambista')->group(function () {
+    Route::get('dashboard', function () {
+        return view('cambista.dashboard');
+    });
+});
+
+/*
+|--------------------------------------------------------------------------
+| Rotas direcionadas a qualquer usuario do sistema
+|--------------------------------------------------------------------------
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', 'UsuarioController@login');
+Route::get('/login', function() {
+    return view('login');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Rotas direcionadas somente aos GERENTES do sistema
+|--------------------------------------------------------------------------
+*/
 Route::post('/login', 'UsuarioController@logar');
